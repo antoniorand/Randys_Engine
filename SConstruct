352d052726dbb1e6b3103afdea317f798d4e4fce
antoniorand <tony.randalph@gmail.com>
2022-02-02 17:48:44 +0200

    added metaprogramming wip

--- SConstruct
+++ SConstruct
@@ -3,9 +3,15 @@ libraries = ['irrlicht']
 pathToLibraries=['./lib']
 
 env = Environment(
-    CPPPATH=['lib/','./src/utils/'],
-    SCONS_CXX_STANDARD='c++20',
+    CPPPATH=['lib/','./src/utils/']
 )
+
+debug = ARGUMENTS.get('debug',0)
+
+if debug == 0:
+    env.Append(CCFLAGS='-O3 -std=c++20')
+else:
+    env.Append(CCFLAGS='-ggdb3 -std=c++20 -Wall -Wpedantic -Wconversion -fsanitize=undefined')
 
 
 app = env.Program(target= 'randysEngine',source = 'src/main.cpp',LIBS = libraries, LIBPATH=pathToLibraries )
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 
-#include <iostream>
-#include <memory>
-#include <memoryPool/mem_Pool.hpp>
+#include "GameEngine/GameEngine.hpp"
+
+struct Entity{
+    uint8_t components_; 
+};
+
+struct CPhysics{ static constexpr uint8_t id{0};};
+struct CRender{ static constexpr uint8_t id{1};};
+struct CHealth{static constexpr uint8_t id{2};};
+
+using ComponentList = generic_GameEngine::Typelist<CPhysics, CRender,  CHealth>;
+
+struct TPlayer{static constexpr uint8_t id{0};};
+struct TEnemy{static constexpr uint8_t id{1};};
+struct TBullet{static constexpr uint8_t id{2};};
+
+using TagList = generic_GameEngine::Typelist<TPlayer,TEnemy,TBullet>;
+
+
+
+//using TN = nth_type<i, Ts...>::type;
 
 int main(){ 
 
-    std::cout << sizeof(Randys_Engine::SMemoryChunk) << std::endl;
+    using namespace generic_GameEngine::MetaProgramming;
 
     return 0;
 }
