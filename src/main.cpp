0d294e2924541d96674159735b231d520c274013
antoniorand <tony.randalph@gmail.com>
2022-02-12 18:29:09 +0200

    Basic Memory Pool Finished

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,19 @@
 #include <memoryPool.hpp>
 #include <iostream>
 
-#define CHECK_BIT_REVERSED(var,pos) (((uint8_t)var) & (0x40 >> pos))
+#define CHECK_BIT_REVERSED(var,pos) (((uint8_t)var) & (0x80 >> pos))
+#define SET_BIT_REVERSED  (var,pos) (((uint8_t)var) | (0x80 >> pos))  
 
 int main(){ 
 
-    //auto& poolilla = RandysEngine::Pool::get_instance<2>();
 
-    //auto ptr1 = poolilla.at(0).allocate(3);
-    //auto ptr2 = poolilla.at(0).allocate(3);
-    //auto ptr3 = poolilla.at(0).allocate(3);
-    //auto ptr4 = poolilla.at(0).allocate(3);
-    //auto ptr5 = poolilla.at(0).allocate(3);
+    auto poolilla = RandysEngine::Pool::Bucket {100,10};
 
+    auto ptr1 = poolilla.allocate(100);
+    auto ptr2 = poolilla.allocate(100);
+    auto ptr3 = poolilla.allocate(100);
+    poolilla.deallocate(ptr2,100);
+    auto ptr4 = poolilla.allocate(100);
 
     return 0;
 }
--- src/utils/memoryPool.cpp
+++ src/utils/memoryPool.cpp
@@ -1,6 +1,6 @@
 #include "memoryPool.hpp"
-#define CHECK_BIT_REVERSED(var,pos) (((uint8_t)var) & (0x40 >> pos))
-#define SET_BIT_REVERSED  (var,pos) (((uint8_t)var) | (0x40 >> pos))  
+#define CHECK_BIT_REVERSED(var,pos) (((uint8_t)var) & (0x80 >> pos))
+#define SET_BIT_REVERSED  (var,pos) (((uint8_t)var) | (0x80 >> pos))  
 
 RandysEngine::Pool::Bucket::Bucket(std::size_t e_block_size, std::size_t e_block_count) : 
     blockSize{e_block_size}, blockCount{e_block_count}{
@@ -15,6 +15,17 @@ RandysEngine::Pool::Bucket::Bucket(std::size_t e_block_size, std::size_t e_block
     assert(m_ledger != nullptr);
     memset(m_data.get(),0,data_size);
     memset(m_ledger.get(),0,ledger_size);
+}
+
+bool RandysEngine::Pool::Bucket::belongs(void * ptr) const noexcept{
+
+    bool devolver = false;
+
+    if(ptr > m_data.get() && ptr < m_data.get()+(blockSize*blockCount))
+        devolver = true;
+
+    return (devolver);
+
 }
 
 std::size_t RandysEngine::Pool::Bucket::find_contiguous_blocks(std::size_t n) const noexcept{
@@ -52,9 +63,9 @@ void RandysEngine::Pool::Bucket::set_blocks_in_use(std::size_t index,std::size_t
     for(std::size_t j = 0; j < n; j++){
         //We need to get the byte by the index
         //Also we need the REFERENCE not a copy, dumbass
-        std::byte& byteToRetrieve = m_ledger.operator[]((n+j)/8);
+        std::byte& byteToRetrieve = m_ledger.operator[]((index+j)/8);
         //Afterwards we have to set the specific bit of the index block
-        byteToRetrieve = (std::byte)(((uint8_t)byteToRetrieve) | (128 >> (n+j)%8));
+        byteToRetrieve = (std::byte)(((uint8_t)byteToRetrieve) | (0x80 >> (index+j)%8));
     }
 
 }
@@ -72,19 +83,31 @@ void* RandysEngine::Pool::Bucket::allocate(std::size_t bytes) noexcept{
         devolver = m_data.get() + (index*blockSize);
     }
 
-    std::cout << index << std::endl;
-
     return devolver;
+}
+
+void RandysEngine::Pool::Bucket::set_blocks_in_free(std::size_t index,std::size_t n) noexcept{
+    //Before calling this function, we are 100% sure that the blocks
+    //we are about to set are ***USED***
+    //don't worry about any error to much tbh
+    for(std::size_t j = 0; j < n; j++){
+        //We need to get the byte by the index
+        //Also we need the REFERENCE not a copy, dumbass
+        std::byte& byteToRetrieve = m_ledger.operator[]((index+j)/8);
+        //Afterwards we have to set the specific bit of the index block
+        byteToRetrieve = (std::byte)(((uint8_t)byteToRetrieve) & ~(0x80 >> (index+j)%8));
+    }
+
 }
 
 void RandysEngine::Pool::Bucket::deallocate(void * ptr, std::size_t bytes) noexcept {
 
     const auto p = static_cast<const std::byte *>(ptr);
     const std::size_t dist = static_cast<std::size_t>(p - m_data.get());
-    //Calculate block indez from pointer distance
+    //Calculate block index from pointer distance
     const auto index = dist/blockSize;
     //Calculate the required number of blocks;
     const auto n = 1 + ((bytes-1)/blockSize);
     //Update the ledger
-    //set_blocks_in_free(index,n);
+    set_blocks_in_free(index,n);
 }
