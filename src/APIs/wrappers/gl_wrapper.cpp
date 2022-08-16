5fd7fe8d166d57e7584d163e6adfa209a02b17c9
antoniorand <tony.randalph@gmail.com>
2022-08-16 12:30:01 +0200

    now it's time to solve that segmentation fault

--- src/APIs/wrappers/gl_wrapper.cpp
+++ src/APIs/wrappers/gl_wrapper.cpp
@@ -129,7 +129,7 @@ namespace RandysEngine{
         
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        //glBindVertexArray(0); // no need to unbind it every time 
+        glBindVertexArray(0); // no need to unbind it every time 
     }
 
 }
