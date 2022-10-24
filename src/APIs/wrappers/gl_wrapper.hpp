046bf5c2f5101cd6ec8566882b300147c290c9db
antoniorand <tony.randalph@gmail.com>
2022-10-24 17:43:59 +0200

    moved dependency GLAD

--- src/APIs/wrappers/gl_wrapper.hpp
+++ src/APIs/wrappers/gl_wrapper.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "base_wrapper.hpp"
-#include "../GLAD/glad.h"
+#include <GLAD/glad.h>
 #include <glm/glm.hpp>
 #include "glm/gtc/matrix_transform.hpp"
 #include <glm/gtc/type_ptr.hpp>
