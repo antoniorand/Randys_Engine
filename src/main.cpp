aaf4cca7e27fe76cb1df585fecdb818a20369600
antoniorand <tony.randalph@gmail.com>
2022-11-04 11:11:54 +0100

    cambios en el main

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,39 @@ int main(){
 
     renderer.addLayerBack<RandysEngine::layer_skybox>();
     renderer.addLayerBack<RandysEngine::layer_minitree>();
+    renderer.addLayerBack<RandysEngine::layer_minitree>();
     
     auto node0  = renderer.createNode<RandysEngine::layer_minitree>(1);
     auto node1 = renderer.createNode<RandysEngine::layer_minitree>(node0);
+    auto node2 = renderer.createNode<RandysEngine::layer_minitree>(2);
 
     auto nodeCamera = renderer.createNode<RandysEngine::layer_minitree>(1);
+    auto nodeCamera2 = renderer.createNode<RandysEngine::layer_minitree>(2);
 
     renderer.addModel<RandysEngine::layer_minitree>(node1);
+    renderer.addModel<RandysEngine::layer_minitree>(node2);
 
     renderer.addCamera<RandysEngine::layer_minitree>(nodeCamera,45.0f,400.0f,240.0f,0.01f,10000.0f);
     renderer.setActiveCamera<RandysEngine::layer_minitree>(nodeCamera);
-
+    renderer.addCamera<RandysEngine::layer_minitree>(nodeCamera2,45.0f,400.0f,240.0f,0.01f,10000.0f);
+    renderer.setActiveCamera<RandysEngine::layer_minitree>(nodeCamera2);
 
 #ifndef __3DS__
     renderer.addTextureSkybox(0,"resources/CubeExample.png");
     unsigned int meshN = renderer.addMesh<RandysEngine::layer_minitree>(node1,"resources/test.obj");
     renderer.addTexture<RandysEngine::layer_minitree>(node1,meshN,"resources/test.png");
+    unsigned int meshN2 = renderer.addMesh<RandysEngine::layer_minitree>(node2,"resources/rowlet.obj");
+    renderer.addTexture<RandysEngine::layer_minitree>(node2,meshN2,"resources/rowlet.png");
 #else
     renderer.addTextureSkybox(0,"romfs:/gfx/CubeExample.t3x");
     unsigned int meshN = renderer.addMesh<RandysEngine::layer_minitree>(node1,"romfs:/3d_resources/test.obj");
     renderer.addTexture<RandysEngine::layer_minitree>(node1,meshN,"romfs:/gfx/test.t3x");
+    unsigned int meshN2 = renderer.addMesh<RandysEngine::layer_minitree>(node2,"romfs:/3d_resources/rowlet.obj");
+    renderer.addTexture<RandysEngine::layer_minitree>(node2,meshN2,"romfs:/gfx/rowlet.t3x");
 #endif
 
-    renderer.setTranslateNode(node1, 0.0f,-1.0f,-15.0f);
+    renderer.setTranslateNode(node1, 0.0f,-1.0f,-5.0f);
+    renderer.setTranslateNode(node2, 3.0f,-1.0f,-10.0f);
 
     while(renderer.isAppRunning()){
         if(renderer.readKeyPressed(RandysEngine::KeyInput::exit)){
